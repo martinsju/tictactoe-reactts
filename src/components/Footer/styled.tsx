fadd6e932b9b5275b1fada7383efc8109c56fbb8
julia <agramartinsj@gmail.com>
2022-12-20 21:22:29 -0300

    style: styled footer

---
+++ src/components/Footer/styled.tsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+export const StyledFooter = styled.footer`
+	height: 50px;
+	width: 100%;
+	display: flex;
+	box-sizing: border-box;
+	position: absolute;
+	bottom: 0;
+	left: 0;
+	justify-content: center;
+	align-items: center;
+	border: 1px solid red;
+`
