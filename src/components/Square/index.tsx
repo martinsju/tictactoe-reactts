1a199cb3b6ca541677701b87e850f0c5848b0a5c
julia <agramartinsj@gmail.com>
2022-12-20 22:37:49 -0300

    feat: create square logic and functions

--- src/components/Square/index.tsx
+++ src/components/Square/index.tsx
@@ -1,7 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { StyledSquare } from './styled'
 
-const Square: React.FC = function () {
-	return <div>Square</div>
+type SquareProps = {
+	turn: string
+	id: string
+	isToggle: boolean
+	setToggle: (isToogle: boolean) => void
+}
+
+const Square: React.FC<SquareProps> = function ({
+	turn,
+	id,
+	setToggle,
+	isToggle
+}) {
+	const [content, setContent] = useState('')
+
+	function handleClick() {
+		console.log('conteudo do quadrado: ', content)
+		if (content === '') {
+			setToggle(!isToggle)
+			setContent(turn)
+		}
+	}
+
+	return <StyledSquare onClick={handleClick}>{content}</StyledSquare>
 }
 
 export default Square
