576d2a08d8aed51523c72af35e3b2f3fc17c9133
julia <agramartinsj@gmail.com>
2022-12-20 21:21:16 -0300

    feat: add component Footer

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { StyledFooter } from './styled'
+
+const Footer: React.FC = function () {
+	return <StyledFooter>Made by Julia Agra and chat {'<'}3 </StyledFooter>
+}
+
+export default Footer
