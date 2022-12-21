113bc3fc387b0b67faa16a3cef64ed581b88f192
julia <agramartinsj@gmail.com>
2022-12-20 22:42:02 -0300

    style: styled Square

---
+++ src/components/Square/styled.tsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+export const StyledSquare = styled.div`
+	height: 35px;
+	width: 35px;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	border: 2px solid black;
+`
