6ca5324fba0da374196963e868c7bf254e67a6e9
julia <agramartinsj@gmail.com>
2022-12-20 22:42:25 -0300

    style: styled Game board

---
+++ src/components/TicTacToe/styled.tsx
@@ -0,0 +1,24 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+`
+
+export const Board = styled.div`
+	display: flex;
+	width: fit-content;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	gap: 3px;
+`
+
+export const Row = styled.div`
+	display: flex;
+	width: fit-content;
+	flex-direction: row;
+	gap: 3px;
+`
